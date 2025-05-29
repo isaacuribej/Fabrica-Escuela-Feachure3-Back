4480f9909359ddf30c785e0c6182afee38b72a3a
root <alejandroorrego0330@gmail.com>
2025-05-29 16:26:25 -0500

    login

--- src/main/java/com/prueba/prueba/Agentes/AgentesRepositorio.java
+++ src/main/java/com/prueba/prueba/Agentes/AgentesRepositorio.java
@@ -3,7 +3,9 @@ package com.prueba.prueba.Agentes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.graphql.data.GraphQlRepository;
+import java.util.Optional;
 
 @GraphQlRepository
 public interface AgentesRepositorio extends JpaRepository<Agentes, Integer>, QueryByExampleExecutor <Agentes> {
+    Optional<Agentes> findByNombreUsuario(String nombreUsuario);
 }
--- src/main/java/com/prueba/prueba/Agentes/AgentesResolver.java
+++ src/main/java/com/prueba/prueba/Agentes/AgentesResolver.java
@@ -1,19 +1,26 @@
 package com.prueba.prueba.Agentes;
 
 
-import com.prueba.prueba.Clientes.Clientes;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+
+import com.prueba.prueba.Utilities.PasswordEncryptor;
+
+
+
 @Controller
 public class AgentesResolver {
     private final  AgentesRepositorio agentesRepositorio;
+    private final PasswordEncryptor passwordEncryptor;
 
-    public AgentesResolver(AgentesRepositorio agentesRepositorio) {
+    public AgentesResolver(AgentesRepositorio agentesRepositorio, PasswordEncryptor passwordEncryptor) {
         this.agentesRepositorio = agentesRepositorio;
+        this.passwordEncryptor = passwordEncryptor;
     }
 
     @QueryMapping
@@ -26,6 +33,38 @@ public class AgentesResolver {
         return  agentesRepositorio.findById(id_agente).orElseThrow(() -> new RuntimeException("Agente no encontrado"));
     }
 
+    public boolean validarLogin(String nombreUsuario, String contrasenaHash) {
+        Agentes agentes = agentesRepositorio.findByNombreUsuario(nombreUsuario).orElse(null);
+        if (nombreUsuario == null) return false;
+        return passwordEncryptor.matches(contrasenaHash, agentes.getContrasenaHash());
+    }
+
+    @MutationMapping(name = "LoginAgente")
+    public Boolean LoginAgente(@Argument String nombreUsuario, @Argument String contrasenaHash) {
+        return validarLogin(nombreUsuario, contrasenaHash);
+    }
+
+
+    public record AgentesInput(
+            String nombreUsuario,
+            String contrasenaHash,
+            String direccion,
+            String telefono,
+            String correoElectronico
+    ) {}
+
+
+    @MutationMapping(name = "insertarAgente")
+    public Agentes insertarAgente(@Argument AgentesInput agentesInput) {
+        Agentes agentes = new Agentes();
+        // Encriptar la contraseña antes de guardar
+        agentes.setContrasenaHash(passwordEncryptor.encrypt(agentesInput.contrasenaHash()));
+        agentes.setNombreUsuario(agentesInput.nombreUsuario());
+        agentes.setDireccion(agentesInput.direccion());
+        agentes.setTelefono(agentesInput.telefono());
+        agentes.setCorreoElectronico(agentesInput.correoElectronico());
+        return agentesRepositorio.save(agentes);
+    }
 }
 
 
